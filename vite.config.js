3251389d4e045b32a81ec45e0b25baaa50ad2e09
mforte215 <mark.fortee@gmail.com>
2024-03-05 08:18:12 -0500

    config change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    open: true,
+    proxy: {
+      '/graphql': {
+        target: 'https://tech-journey-apollo-2137197654ec.herokuapp.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
